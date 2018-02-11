cd5b3f4906a265c7582374b619dc0f4f1b3db9b6
Gopal <gopalkurup@gmail.com>
2018-02-11 22:58:04 +0530

    Minor change

--- resources/views/auth/registerNew.blade.php
+++ resources/views/auth/registerNew.blade.php
@@ -4,8 +4,9 @@
 <style>
   
 </style>
-<h1 id="heading">Register New</h1>
-<div class="row">
+<div class="row rym">
+<h1 id="heading" align="center">Register New</h1>
+
     <div class="col-md-6 col-md-offset-3">
         <div class="panel panel-default">
             <div class="panel-body">
@@ -19,7 +20,7 @@
            
             <div class="form-group">
                 
-                {{Form::radio('college','home',false, ['id' => 'home' , 'onchange' => 'hideother()','required'])}}College of Engineering Trivandrum <br>
+                {{Form::radio('college','home',false, ['id' => 'home' , 'onchange' => 'hideother()','required'])}}Home <br>
                 {{Form::radio('college','others',false, ['id' => 'other' , 'onchange' => 'displayother()','required'])}}Others
             </div>
             <div class="form-group">
@@ -27,7 +28,7 @@
                 {{Form::text('collegename','',['class' => 'form-control','placeholder' => 'College', 'id' => 'inputother'])}}
             </div>
             
-            {{Form::submit('Submit',['class' => 'btn btn-success'])}}
+            {{Form::submit('Submit',['class' => 'btn btn-success editb'])}}
         {!! Form::close() !!}
         </div>
         </div>
